42cf75b9ce27c2e1a3bf6bba0ec04669ae7a6b2e
Ryan <rlmcmillan12@gmail.com>
2022-04-06 21:43:32 -0400

    finished class exercises from 2022-04-06

--- classExercises4-6-22.py
+++ classExercises4-6-22.py
@@ -3,6 +3,7 @@
 # first_name = 'Ryan'
 # last_name = 'McMillan'
 
+
 # # Assign some string values to the variables. You can use your own name or someone else's.
 # # Use those two variables to print a greeting that uses the firstname and lastname variables in the output.
 
@@ -33,42 +34,41 @@
 
 # Working with functions
 
-
+# inputs
 first_name = input('What is your first name?: ')
 last_name = input('What is your last name?: ')
-domain = input('What is your domain name?:')
+domain = '@mcmillancarpentry.com'
 # Greeter
 
 
 def greeter(first_name, last_name):
+    print(f"Thank you and welcome to the woodshop, {first_name} {last_name}")
 
-    #     print(f"Thank you and welcome to the woodshop, {first_name} {last_name}")
-
-    # greeter()
+    # greeter(first_name, last_name)
 
     # Email generator
 
 
 def email_generator(first_name, last_name, domain):
-    #     print(f"{first_name[0:1].lower()}.{last_name.lower()}@{domain}")
+    print(f"Email: {first_name[0:1].lower()}.{last_name.lower()}@{domain}")
 
-    # email_generator('Ryan', 'McMillan', 'mcmillancarpentry.com')
+    # email_generator(first_name, last_name, domain)
 
     # Username generator
 
 
 def username_generator(first_name, last_name):
-    #     print(f"{first_name[0:3].lower()}_{last_name[0:5].lower()}")
+    print(f"Username: {first_name[0:3].lower()}_{last_name[0:5].lower()}")
 
-    # username_generator('Ryan', 'McMillan')
+    # username_generator(first_name, last_name)
 
     # New user contact information
 
 
 def new_user_contact_info(first_name, last_name, domain):
-    print(f"""welcome to the woodshop, {first_name} {last_name}
-    Email: {first_name[0:1].lower()}.{last_name.lower()}@{domain}
-    Username: {first_name[0:3].lower()}_{last_name[0:5].lower()}""")
+    greeter(first_name, last_name)
+    email_generator(first_name, last_name, domain)
+    username_generator(first_name, last_name)
 
 
-new_user_contact_info('Ryan', 'McMillan', 'mcmillancarpentry.com')
+new_user_contact_info(first_name, last_name, domain)
