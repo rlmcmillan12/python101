ee675eb973fb97f64140b02300fe146d7ab29c7a
Ryan <rlmcmillan12@gmail.com>
2022-04-12 12:42:45 -0400

    creat folder and file for in class exercise

--- 2022-04-12_in_class_abstraction/main.py
+++ 2022-04-12_in_class_abstraction/main.py
@@ -1,4 +1,4 @@
-from dragonball import print_basic_stats
+from dragonball import print_basic_stats, dragonball_dictionary
 
 
-print_basic_stats(character[2])
+print_basic_stats(dragonball_dictionary[1])
