341d24954710317219f522841b969e3a38890c46
Ryan <rlmcmillan12@gmail.com>
2022-04-12 11:16:05 -0400

    changed to do list homework

--- 2022_04_11_in_class_Dictionaries.py
+++ 2022_04_11_in_class_Dictionaries.py
@@ -1,20 +1,20 @@
 # # Accessing Data
 
-phonebook_dict = {
-    "Alice": "703-493-1834",
-    "Bob": "857-384-1234",
-    "Elizabeth": "484-584-2923",
-}
+# phonebook_dict = {
+#     "Alice": "703-493-1834",
+#     "Bob": "857-384-1234",
+#     "Elizabeth": "484-584-2923",
+# }
 
-# print(phonebook_dict["Elizabeth"])
-phonebook_dict["Kareem"] = "938-489-1234"
-phonebook_dict.pop("Alice")
-phonebook_dict["Bob"] = "986-345-2345"
-# print(phonebook_dict)
+# # print(phonebook_dict["Elizabeth"])
+# phonebook_dict["Kareem"] = "938-489-1234"
+# phonebook_dict.pop("Alice")
+# phonebook_dict["Bob"] = "986-345-2345"
+# # print(phonebook_dict)
 
-for i in phonebook_dict:
-    print(i)
-    print(phonebook_dict[i])
+# for i in phonebook_dict:
+#     print(i)
+#     print(phonebook_dict[i])
 
 
 # Nested Dictionaries
@@ -46,19 +46,19 @@ for i in phonebook_dict:
 # #how many times each letter in the alphabet was used in the word
 
 
-def letter_history(user_word):
-    letter_count_dict = {}
+# def letter_history(user_word):
+#     letter_count_dict = {}
 
-    for letter in user_word:
-        if letter not in letter_count_dict:
-            letter_count_dict[letter] = 0
-        letter_count_dict[letter] += 1
-    print(letter_count_dict)
+#     for letter in user_word:
+#         if letter not in letter_count_dict:
+#             letter_count_dict[letter] = 0
+#         letter_count_dict[letter] += 1
+#     print(letter_count_dict)
 
 
-user_word = input("Enter a word and I will do my magic: ")
+# user_word = input("Enter a word and I will do my magic: ")
 
-letter_history(user_word)
+# letter_history(user_word)
 
 
 ## Word Histogram
@@ -67,15 +67,15 @@ letter_history(user_word)
 # times each word in the alphabet was used in the text.
 
 
-def word_histogram(user_sentence):
-    user_sentence_list = user_sentence.split()
-    word_count_dict = {}
+# def word_histogram(user_sentence):
+#     user_sentence_list = user_sentence.split()
+#     word_count_dict = {}
 
-    for words in user_sentence_list:
-        if words not in word_count_dict:
-            word_count_dict[words] = 0
-        word_count_dict[words] += 1
-    print(word_count_dict)
+#     for words in user_sentence_list:
+#         if words not in word_count_dict:
+#             word_count_dict[words] = 0
+#         word_count_dict[words] += 1
+#     print(word_count_dict)
 
 
 # user_sentence = input("Type a sentence and I will so my magic: ")
@@ -88,31 +88,31 @@ def word_histogram(user_sentence):
 # print the top 3 words or letters:
 
 
-# def word_histogram(user_sentence, sorted_word_count_dict):
-#     user_sentence_list = user_sentence.split()
-#     word_count_dict = {}
-#     for words in user_sentence_list:
-#         if words not in word_count_dict:
-#             word_count_dict[words] = 0
-#         word_count_dict[words] += 1
-#     sorted_word_count_dict = sorted(
-#         word_count_dict, key=word_count_dict.get, reverse=True
-#     )
-#     return sorted_word_count_dict
+def word_histogram(user_sentence, word_count_dict):
+    user_sentence_list = user_sentence.split()
+    word_count_dict = {}
+    for words in user_sentence_list:
+        if words not in word_count_dict:
+            word_count_dict[words] = 0
+        word_count_dict[words] += 1
+    return word_count_dict
 
 
-# def top3Sorter(user_sentence, sorted_word_count_dict):
-#     top3 = {}
-#     for word in sorted_word_count_dict:
-#         for word in sorted_word_count_dict:
-#             top3[word] = user_sentence[word]
-#     print(sorted_word_count_dict)
+def top3Sorter(word_count_dict):
+    top3 = {}
+
+    sorted_word_count_dict = sorted(
+        word_count_dict, key=word_count_dict.get, reverse=True
+    )[:3]
+
+    # for word in sorted_word_count_dict:
+    #     top3[word] = sorted_word_count_dict[word]
+    print(sorted_word_count_dict)
 
 
-# user_sentence = input("Type a sentence and I will so my magic: ")
-# sorted_word_count_dict = {}
-# word_histogram(user_sentence, sorted_word_count_dict)
-# top3Sorter(user_sentence, sorted_word_count_dict)
+user_sentence = input("Type a sentence and I will so my magic: ")
+word_count_dict = {}
+word_histogram(user_sentence, word_count_dict)
+top3Sorter(word_count_dict)
 
-
-#
+# top3Sorter(sorted_word_count_dict)
--- To_Do_List_APP/2022_04_11_Homework_ToDoList.py
+++ To_Do_List_APP/2022_04_11_Homework_ToDoList.py
@@ -1,47 +1,51 @@
 ## TO DO LIST:
 
 
-def To_Do_List():
-    to_do = {}
+def print_to_do(to_do):
+    for i in to_do:
+        print(f'{to_do.index(i + 1)} - {i["task"]} - {i["priority"]} ')
+
+
+def To_Do_List(to_do):
     while True:
 
-        menu_selection = int(
-            input(
-                "Press 1 to add a task\nPress 2 to delete a task\nPress 3 to view all tasks\nPress 4 to quit\nEnter your option now: "
-            )
+        menu_selection = input(
+            "Press 1 to add a task\nPress 2 to delete a task\nPress 3 to view all tasks\nPress 4 to quit\nEnter your option now: "
         )
 
-        if menu_selection == 4:
+        if menu_selection == "4":
             break
-        if menu_selection == 1:
+        if menu_selection == "1":
             task = input("What is your task?: ")
-            priority_select = int(
-                input("Press 1 for high priority\nPress 2 for low priority: ")
+            priority_select = input(
+                "Press 1 for high priority\nPress 2 for medium priority\nPress 3 for low: "
             )
-            if priority_select == 1:
+            if priority_select == "1":
                 priority = "High"
-                to_do[task] = priority
+            elif priority_select == "2":
+                priority = "Medium"
             else:
                 priority = "Low"
-                to_do[task] = priority
-        elif menu_selection == 2:
-            count = 0
-            for i in to_do:
-                count += 1
-                print(f"{count} - {i} - {to_do[i]}")
-            del_task = input("Which task would you like removed?: ")
-            del to_do[del_task]
+            current_task = {"task": task, "priority": priority}
+            to_do.append(current_task)
+        print_to_do(to_do)
 
-        elif menu_selection == 3:
-            count = 0
-            for i in to_do:
-                count += 1
-                print(str(count) + " - " + str(i) + " - " + to_do[i])
+        # elif menu_selection == "2":
+        #     count = 0
+        #     for i in to_do:
+        #         count += 1
+        #         print(f"{count} - {i} - {to_do[i]}")
+        #     del_task = input("Which task would you like removed?: ")
+        #     del to_do[del_task]
+
+        # elif menu_selection == 3:
+        #     count = 0
+        #     for i in to_do:
+        #         count += 1
+        #         print(str(count) + " - " + str(i) + " - " + to_do[i])
+        # else:
+        #     print("\nPlease enter a valid entry")
 
 
-# delete, task, view all tasks, quit
-# assign priority to each task
-# print high to low priority
-
-
-To_Do_List()
+to_do = []
+To_Do_List(to_do)
